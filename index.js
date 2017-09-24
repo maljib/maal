41d6a43d1d9c258f07e06aa34751d4750fd26eaa
huang <huang.degil@gmail.com>
2017-09-24 20:20:24 +0900

    메뉴와 항목 간격 줄임

--- index.js
+++ index.js
@@ -1153,7 +1153,7 @@ $(function() {
           }
   	    }
       }
-    }).position({my:lr+" top-"+$(to).offset().top, at:lr+" bottom", of:to});
+    }).position({my:lr+" top-"+$(to).offset().top, at:lr+" bottom-2", of:to});
 
     $(menu +","+ to).hover(function() {
       $(menu).show();
