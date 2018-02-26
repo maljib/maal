cf6a7193d557ffc11ed308813437c1334312f9e1
huang <huang.degil@gmail.com>
2018-02-26 20:30:24 +0900

    글틀얼개 - 올림말 찾기 물음 고침

--- search.php
+++ search.php
@@ -1,43 +1,43 @@
 <?php // search.php
 require_once 'functions.php';
 
-$c = 'DISTINCT w.word';     // selected columns
-$j = '';                    // join
-$o = 'ORDER BY w.word';     // order
-$d = '';                    // 역순인가?
-if ($w = getPost('l')) {    // l이 들어있는 올림말
-  $w = "w.word LIKE '%".escapeString($w)."%' AND ";
+$s = 'DISTINCT w.word';     // selected columns
+$j = '';                    // join clauses
+$o = 'ORDER BY w.word';     // ordering instruction
+$d = '';                    // ' DESC' = 역순
+if ($w = getPost('l')) {    // where clause
+  $w = "w.word LIKE '%".escapeString($w)."%' AND";  // l이 들어있는 올림말
 } else {
-  if ($w = getPost('w')) {  // w부터 또는 w까지
-    if ($w{0} === '-') {    // 역순인가?
-      $d = ' DESC';
-      $w = substr($w, 1);
+  if ($w = getPost('w')) {  // where clause
+    if ($w{0} === '-') {
+      $d = ' DESC';         // 역순
+      $w = substr($w,1);
     }
-    if ($w) {
-      $w = 'w.word '.($d? '<': '>')."= '".escapeString($w)."' AND ";
+    if ($w) {               // $w부터 가나다 순 또는 $w부터 가나다 역순
+      $w = 'w.word '.($d? '<': '>')."= '".escapeString($w)."' AND";
     }
   }
   if ($x = getPost('x')) {
-    $s = $x{0};              // @ # $ ^ & --> a e m 1 2
-    $j = " JOIN users u ON u.nick = '".escapeString(substr($x, 1))."' AND u.id = ";
-    if ($s == 'e' || $s == 'm') {
-      $j = 'JOIN texts e ON w.id = e.word AND e.i='.
-           ($s == 'e'? '0 AND w.user <> e.user': '1').$j.'e.user';
+    $j = "JOIN users u ON u.nick='".escapeString(substr($x,1))."' AND u.id=";
+    $x = $x{0};   // @ # $ ^ & --> a e m 1 2
+    if ($x == 'm' || $x == 'e') {  // 적바림 또는 다른 사람이 적은 자취
+      $j = 'JOIN texts e ON w.id=e.word AND e.i='.
+           ($x == 'm'? '1': '0 AND w.user<>e.user')." $j e.user";
     } else {
-      $j .= 'w.user';
-      if ($s != 'a') {
-        $j .= ' AND w.tell = '.$s;
+      $j .= 'w.user';              // 올림말 올린이
+      if ($x != 'a') {
+        $j .= " AND w.tell=$x";    // $x: 1=올림 2=버림
       }
-      if (!$w) {
-        $j .= ' JOIN texts e ON w.id = e.word AND e.i=0 AND e.user = w.user';
+      if (!$w) {  // $w가 없으면 맨 나중에 / 처음에 넣은 것 부터
+        $j .= ' JOIN texts e ON w.id=e.word AND e.i=0 AND e.user=w.user';
       }
     }
-    if (!$w) {
-      $c = 'w.word, max(e.t) tt';
-      $o = 'GROUP BY w.word ORDER BY tt';
+    if (!$w) {    // $w가 없으면 맨 나중에 / 맨 처음에 넣은 것 부터
+      $s = 'w.word, max(e.t) max_t';
+      $o = 'GROUP BY w.word ORDER BY max_t';
       $d = $d? '': ' DESC';
     }
   }
 }
-echo json_encode(selectValues($c, "words w $j", "$w w.word<>'?' $o$d LIMIT 200"));
+echo json_encode(selectValues($s, "words w $j", "$w w.word<>'?' $o$d LIMIT 200"));
 ?>
