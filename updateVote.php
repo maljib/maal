327e3a10dc64cd0498045425f1bb311f7843ad2c
Huang Degil <huang.degil@gmail.com>
2019-09-04 08:24:45 +0900

    Prevented the timestamp from being updated on vote

--- updateVote.php
+++ updateVote.php
@@ -2,5 +2,5 @@
 require_once 'functions.php';
 $a = $_POST['a'];
 $s = escapeString($_POST['s']);
-echo sqlUpdate('deals', "vote='$s'", "id=$a");
+echo sqlUpdate('deals', "vote='$s',t=t", "id=$a");
 ?>
