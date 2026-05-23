ca6f9fdf41783bd966d4314896f1efb3054e7967
Huang Degil <huang.degil@gmail.com>
2026-05-23 10:25:56 +0900

    Hangul Dictionarry Sort refactored

--- test.php
+++ test.php
@@ -20,56 +20,56 @@ for ($c = count($as), $i = 0; $i < $c; $i++) {
   $as[$i][] = array($i, $i+1);
 }
 print_r($as);
-
+print("\n");
 
 $collator = new Collator('root');
 $collator->setStrength(Collator::PRIMARY);
-$collator->sort($items);
 
-function getSortKey($str) {
+$choseong =  ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ',
+              'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+$interval =  (0xD7A4 - 0xAC00) / count($choseong);
+
+// 한글 사전식 정렬을 위한 키를 구하는 함수
+function getHangulDictionarySortKey($str) {
   if ($str === '') {
-    return ['', 0, ''];
+    return ['', 0, ''];                                 // ""
   }
-  
-  $first_char = mb_substr($str, 0, 1, 'UTF-8');
-  $code_point = mb_ord($first_char, 'UTF-8');
-  
-  // Standalone Jamo
-  if (0x3131 <= $code_point && $code_point <= 0x3163) {
+  global $choseong, $interval;  // 초성 목록, 초성 하나로 시작하는 한글 음절 글자 수
+  $first_char = mb_substr($str, 0, 1, 'UTF-8');  // 첫 글자
+  $code_point = mb_ord($first_char, 'UTF-8');    // 첫 글자 코드 포인트
+
+  if (0x3131 <= $code_point && $code_point < 0x3164) {  // 첫 글자가 한글 자모
     return [$first_char, 0, $str];
   }
-
-  // Hangul Syllables
-  if (0xAC00 <= $code_point && $code_point <= 0xD7A3) {
-    $choseong_list = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ',
-                      'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
-    return [$choseong_list[(int) (($code_point - 0xAC00) / 588)], 1, $str];
+  if (0xAC00 <= $code_point && $code_point < 0xD7A4) {  // 첫 글자가 한글 음절 글자
+    return [$choseong[(int) (($code_point - 0xAC00) / $interval)], 1, $str];
   }
-  
-  // Non-Hangul Characters (Using Hangul Filler to position them after 'ㅣ')
-  return ["\u{3164}", 2, $str];
+  return ["\u{3164}", 2, $str];                         // 첫 글자가 한글이 아님
 }
 
 $compare = function($a, $b) use ($collator) {
-  $key_a = getSortKey($a);
-  $key_b = getSortKey($b);
+  $key_a = getHangulDictionarySortKey($a);  // [(초성) 자모, 유형, 원본 문자열]
+  $key_b = getHangulDictionarySortKey($b);  // 유형: 0=자모, 1=음절, 2=한글이 아님
 
   if ($key_a[0] !== $key_b[0]) {
-    return $key_a[0] <=> $key_b[0];
+    return $key_a[0] <=> $key_b[0];  // "", 한글, 한글이 아닌 것 순으로 정렬
   }
   if ($key_a[1] === 2) {
-    return $collator->compare($key_a[2], $key_b[2]);
-    // return strcasecmp($key_a[2], $key_b[2]);
+    return $collator->compare($key_a[2], $key_b[2]);  // 한글이 아님 - 사전식 정렬
   }
   if ($key_a[1] !== $key_b[1]) {
-    return $key_a[1] <=> $key_b[1];
+    return $key_a[1] <=> $key_b[1];  // 자모, 한글 음절 글자 순으로 정렬
   }
-  return $key_a[2] <=> $key_b[2];
+  return $key_a[2] <=> $key_b[2];    // 한글 정렬
 };
 
-$items = ['ㅏ', 'ㄾ', '감', 'P', 'a', 'ㄴ', 'x', '가', 
-          'ㄱ', '나', 'ㄷ', 'orange', 'Apple', 'ㅣ', 'ㄳ', 'ㅄ', 'ㄲ'];;
-print(implode(", ", $items)."\n");
-usort($items, $compare);
-print(implode(", ", $items)."\n");
+$list = ['ㅣ', 'x', 'ㅏ', '달', 'ㄾ', '감', 'P', 'a', 'ㄴ', '가', 'ㄱ',
+           'à', '나', 'ㄷ', 'orange', 'Apple', 'ㄳ', 'ㅄ', 'Á', 'ㄲ'];
+print(implode(", ", $list)."\n");
+
+$collator->sort($list);
+print(implode(", ", $list)."\n");
+
+usort($list, $compare);
+print(implode(", ", $list)."\n");
 ?>
